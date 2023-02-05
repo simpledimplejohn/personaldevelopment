731f68651d49ab8853ae6759cfdf98b56e2b71d3
Simple Dimple John <simpledimplejohn@gmail.com>
2023-02-05 11:20:58 -0500

    update triangle names for consistency

--- __tests__/triangle.test.js
+++ __tests__/triangle.test.js
@@ -9,25 +9,25 @@ is a triangle and is not a triangle
 describe('Triangle', () => {
 
   test('should correctly create a triangle object with three lengths', () => {
-    const triangle = new Triangle(2,4,5);
-    expect(triangle.side1).toEqual(2);
-    expect(triangle.side2).toEqual(4);
-    expect(triangle.side3).toEqual(5);
+    const newTriangle = new Triangle(2,4,5);
+    expect(newTriangle.side1).toEqual(2);
+    expect(newTriangle.side2).toEqual(4);
+    expect(newTriangle.side3).toEqual(5);
   });
 });
 
 describe('Triangle', () => {
   test('testing isTriangle function in constructor', () => {
-    const notTriangle = new Triangle(3,9,22);
-    expect(notTriangle.isTriangle).toEqual("not a triangle");
+    const newTriangle = new Triangle(3,9,22);
+    expect(newTriangle.isTriangle).toEqual("not a triangle");
 
   }); 
 });
 
 describe('Triangle', () => {
   test('testing isTriangle with a good triangle', () => {
-    const notTriangle = new Triangle(3,4,5);
-    expect(notTriangle.isTriangle).toEqual("this is a triangle");
+    const newTriangle = new Triangle(3,4,5);
+    expect(newTriangle.isTriangle).toEqual("this is a triangle");
 
   }); 
 });
@@ -35,29 +35,29 @@ describe('Triangle', () => {
 
 describe('Triangle', () => {
   test('testing the function isEquilateral', () => {
-    const equilateralTriangle = new Triangle(3,3,3);
-    expect(equilateralTriangle.isEquilateral).toEqual("equilateral");
+    const newTriangle = new Triangle(3,3,3);
+    expect(newTriangle.isEquilateral).toEqual("equilateral");
   })
 })
 
 describe('triangle', () => {
   test('testing isEquilateral for not an Equilateral', () => {
-    const notEquilateralTriangle = new Triangle(1,2,3);
-    expect(notEquilateralTriangle.isEquilateral).toEqual("not equilateral");
+    const newTriangle = new Triangle(1,2,3);
+    expect(newTriangle.isEquilateral).toEqual("not equilateral");
   });
 });
 
 describe('triangle', () => {
   test('testing isScalene triangle works', () => {
-    const notIsoscelesTriangle = new Triangle(1,2,3);
-    expect(notIsoscelesTriangle.isScalene).toEqual("scalene");
+    const newTriangle = new Triangle(1,2,3);
+    expect(newTriangle.isScalene).toEqual("scalene");
   });
 });
 
 describe('triangle', () => {
   test('testing isScalene triangle works', () => {
-    const notIsoscelesTriangle = new Triangle(2,2,3);
-    expect(notIsoscelesTriangle.isScalene).toEqual("not scalene");
+    const newTriangle = new Triangle(2,2,3);
+    expect(newTriangle.isScalene).toEqual("not scalene");
   });
 });
 
