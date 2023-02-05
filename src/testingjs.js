78e52affae12f9587ab837748b15d6b446245174
Simple Dimple John <simpledimplejohn@gmail.com>
2023-02-05 10:48:39 -0500

    update practice file

--- src/testingjs.js
+++ src/testingjs.js
@@ -8,23 +8,31 @@ function Triangle(side1, side2, side3, height) {
       ? "not a triangle" : "this is a triangle"; 
       // ternary operator means: condition ? true : fail
     this.isEquilateral  = ((this.side1 == this.side2) 
-      & (this.side1 == this.side3)
-      & (this.side2 == this.side3))
+      & (this.side1 === this.side3)
+      & (this.side2 === this.side3))
       ? "equilateral" : "not equilateral";
     this.isScalene = ((this.side1 != this.side2)
-    & (this.side1 != this.side3)
-    & (this.side2 != this.side3))
+    & (this.side1 !== this.side3)
+    & (this.side2 !== this.side3))
     ? "scalene" : "not scalene";
     this.isIsosceles = (
-      ((this.side1 == this.side2) & (this.side1 == this.side3))
-    || ((this.side2 == this.side1) & (this.side2 == this.side3)))
-    ? "is isosceles" : "is not isosceles";
+      ((this.side1 === this.side2) & (this.side1 === this.side3))
+    || ((this.side1 === this.side2) & (this.side2 === this.side3))
+    || (())
+    ) ? "is isosceles" : "is not isosceles";
 }
-
 const notTriangle = new Triangle(1,1,10);
 const equilateral = new Triangle(3,3,3);
 const isosceles = new Triangle(3,3,5);
 const scalene = new Triangle(1,2,3);
 
+console.log("testing isosceles, answer: <", isosceles.isIsosceles, "> CORRECT??");
+console.log("testing isosceles, answer: <", notTriangle.isIsosceles, "> INCORRECT??");
 
-console.log("testing isTriangle, answer: <", notTriangle.isTriangle, ">");
+
+// console.log("testing isTriangle, answer: <", notTriangle.isTriangle, ">");
+// console.log("testing equilateral, answer: <", equilateral.isEquilateral, ">");
+// console.log("testing equilateral, answer: <", notTriangle.isEquilateral, ">");
+// console.log("\n");
+// console.log("testing equilateral, answer: <", notTriangle.isEquilateral, ">");
+// console.log("\n");
