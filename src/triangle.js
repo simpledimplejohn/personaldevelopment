7914ce52b10ec786d7e51488818284c337b30d06
Simple Dimple John <simpledimplejohn@gmail.com>
2023-02-07 08:15:26 -0500

    update triangle to take classes

--- src/triangle.js
+++ src/triangle.js
@@ -1,3 +1,27 @@
+// This is the new ES6 way to write using a constructor and class
+
+export default class Triangle {
+  constructor(side1, side2, side3) {
+    this.side1 = side1;
+    this.side2 = side2;
+    this.side3 = side3;
+  }
+  checkType() {
+    if ((this.side1 > (this.side2 + this.side3)) || 
+        (this.side2 > (this.side1 + this.side3)) ||
+        (this.side3 > (this.side1 + this.side2))
+        ) {
+          return "this is not a triangle";
+      } else if ((this.side1 !== this.side2) && (this.side1 !== this.side3) && (this.side2 !== this.side3)) { 
+        return "this is a scalene triangle";
+    } else {
+      return "isosceles triangle"
+    }
+  }
+}
+
+/*
+This is the old fashion way of writing this: 
 export default function Triangle(side1, side2, side3) {
   this.side1 = side1;
   this.side2 = side2;
