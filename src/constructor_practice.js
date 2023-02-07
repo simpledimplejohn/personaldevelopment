abb474eb27ed40b2f70e6c9be83125b8ce5d4abc
Simple Dimple John <simpledimplejohn@gmail.com>
2023-02-07 08:05:30 -0500

    add template litteral

---
+++ src/constructor_practice.js
@@ -0,0 +1,13 @@
+// Using arrow functions
+const name = "john"
+
+const greeting = name => "hi " + name;
+
+// console.log(greeting());
+
+// ah template literals--love these
+
+let language = "JavaScript"
+let adjective = "fun"
+
+console.log(`This practice of ${language} is so much ${adjective} `)
